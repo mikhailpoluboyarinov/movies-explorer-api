77cfd25c50df267b0e5b41d6d24ebc73350ab5d5
Mikhail Poluboyarinov <mikhailpoluboyarinov@yandex.ru>
2022-11-27 19:28:50 +0300

    Поправил login

--- controllers/users.js
+++ controllers/users.js
@@ -80,7 +80,7 @@ module.exports.login = (req, res, next) => {
   User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key', { expiresIn: '7d' });
-      res.send({ token });
+      res.send({ token, user });
     })
     .catch(next);
 };

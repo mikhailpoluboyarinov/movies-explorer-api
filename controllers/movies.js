698c818a609359c6151f5715d289a4105fcb0d27
Mikhail <mikhailpoluboyarinov@yandex.ru>
2022-12-14 17:04:54 +0300

    исправил поле id фильма в контроллере

--- controllers/movies.js
+++ controllers/movies.js
@@ -27,7 +27,7 @@ module.exports.deleteMovie = (req, res, next) => {
   const { movieId } = req.params;
   const ownerId = req.user._id;
 
-  Movie.findById({ _id: movieId })
+  Movie.findById({ movieId })
     .then((movie) => {
       if (movie === null) {
         return next(new NotFoundError('Передан несуществующий _id фильма.'));
